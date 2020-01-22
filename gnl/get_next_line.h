a479f487f331f59bb4bd84947879b412e55e6102
apita-da <55708474+apita-da@users.noreply.github.com>
2020-01-22 17:06:37 +0100

    Add files via upload

---
+++ gnl/get_next_line.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apita-da <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/05 14:24:35 by apita-da          #+#    #+#             */
+/*   Updated: 2019/12/05 16:34:25 by apita-da         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <string.h>
+
+int	get_next_line(int fd, char **line);
+
+#endif
