423e16ed696b78a1544d7b02803d355e71bde6b3
apita-da <55708474+apita-da@users.noreply.github.com>
2020-02-19 17:29:15 +0100

    Add files via upload

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -3,65 +3,65 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: apita-da <apita-da@student.42.fr>          +#+  +:+       +#+        */
+/*   By: apita-da <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/12/05 14:23:41 by apita-da          #+#    #+#             */
-/*   Updated: 2020/02/11 17:00:09 by apita-da         ###   ########.fr       */
+/*   Created: 2020/02/19 17:26:57 by apita-da          #+#    #+#             */
+/*   Updated: 2020/02/19 17:27:04 by apita-da         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static int	get_line(int fd, char **s, char **line, int rd)
+static int	get_line(int fd, char **str, char **line, int rd)
 {
 	char	*tmp;
 	char	*nl;
 
-	if (!s[fd] && !rd)
+	if (!str[fd] && !rd)
 	{
 		*line = ft_strdup("");
 		return (0);
 	}
-	if ((nl = ft_strchr(s[fd], '\n')))
+	if ((nl = ft_strchr(str[fd], '\n')))
 	{
 		*nl = '\0';
-		*line = ft_strdup(s[fd]);
+		*line = ft_strdup(str[fd]);
 		tmp = ft_strdup(++nl);
-		free(s[fd]);
-		s[fd] = tmp;
+		free(str[fd]);
+		str[fd] = tmp;
 		return (1);
 	}
 	else
 	{
-		*line = s[fd];
-		s[fd] = NULL;
+		*line = str[fd];
+		str[fd] = NULL;
 		return (0);
 	}
 }
 
-int				get_next_line(int fd, char **line)
+int			get_next_line(int fd, char **line)
 {
 	char		buff[BUFFER_SIZE + 1];
-	static char	*s[4096];
+	static char	*str[4096];
 	int			rd;
 	char		*temp;
 
 	if (!line || fd < 0 || fd > 4095 || (rd = read(fd, buff, 0)) == -1
-			|| BUFFER_SIZE < 1)
+			|| BUFFER_SIZE + 1 < 1)
 		return (-1);
-	while ((rd = read(fd, buff, BUFFER_SIZE)) > 0)
+	while ((rd = read(fd, buff, BUFFER_SIZE + 1)) > 0)
 	{
 		buff[rd] = '\0';
-		if (s[fd] == NULL)
-			s[fd] = ft_strdup("");
-		temp = ft_strjoin(s[fd], buff);
-		free(s[fd]);
-		s[fd] = temp;
-		if (ft_strchr(s[fd], '\n'))
+		if (str[fd] == NULL)
+			str[fd] = ft_strdup("");
+		temp = ft_strjoin(str[fd], buff);
+		free(str[fd]);
+		str[fd] = temp;
+		if (ft_strchr(str[fd], '\n'))
 			break ;
 	}
 	if (rd < 0)
 		return (-1);
-	rd = get_line(fd, s, line, rd);
+	rd = get_line(fd, str, line, rd);
 	return (rd);
 }
